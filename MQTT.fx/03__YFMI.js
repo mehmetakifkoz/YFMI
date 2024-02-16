ab973698bc7c313c305c1dd234a6c34a3c98aa14
Mehmet Akif KÖZ <mehmet_akif_koz@hotmail.com>
2024-02-16 09:05:36 +0300

    Rename MQTT folder as MQTT.fx

--- MQTT.fx/03__YFMI.js
+++ MQTT.fx/03__YFMI.js
@@ -40,20 +40,20 @@ function sendPayload(sample) { // Receive sample as an argument
 }
 
 function toIsoString(date) {
-var tzo = -date.getTimezoneOffset(),
-    dif = tzo >= 0 ? '+' : '-',
-    pad = function(num) {
-        return (num < 10 ? '0' : '') + num;
-    };
+    var tzo = -date.getTimezoneOffset(),
+        dif = tzo >= 0 ? '+' : '-',
+        pad = function(num) {
+            return (num < 10 ? '0' : '') + num;
+        };
 
-return date.getFullYear() +
-    '-' + pad(date.getMonth() + 1) +
-    '-' + pad(date.getDate()) +
-    'T' + pad(date.getHours()) +
-    ':' + pad(date.getMinutes()) +
-    ':' + pad(date.getSeconds()) +
-    dif + pad(Math.floor(Math.abs(tzo) / 60)) +
-    ':' + pad(Math.abs(tzo) % 60);
+    return date.getFullYear() +
+        '-' + pad(date.getMonth() + 1) +
+        '-' + pad(date.getDate()) +
+        'T' + pad(date.getHours()) +
+        ':' + pad(date.getMinutes()) +
+        ':' + pad(date.getSeconds()) +
+        dif + pad(Math.floor(Math.abs(tzo) / 60)) +
+        ':' + pad(Math.abs(tzo) % 60);
 }
 
 function out(message) {
